86ac0ccb6e03d7a883e186a6c76d5e3f0c631909
Sumi <sumi.n.lcy@gmail.com>
2019-02-07 09:23:01 -0700

    iroiro

--- GraphicProject/Source.cpp
+++ GraphicProject/Source.cpp
@@ -1,15 +1,31 @@
-﻿#include <iostream>
+﻿#include "math_3d.h"
+#include "cyCodeBase/cyTriMesh.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 int main()
 {
-	// GLFW を初期化する
+	// Initialize GLFW
 	if (glfwInit() == GL_FALSE)
 	{
 		// 初期化に失敗した
 		std::cerr << "Can't initialize GLFW" << std::endl;
 		return 1;
 	}
-	// ウィンドウを作成する
+
+	//Register a function which will execute when the program finied 
+	atexit(glfwTerminate);
+
+	//Select OpenGL Version 3.2 Core Profile
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Creating a window
 	GLFWwindow *const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
 	if (window == NULL)
 	{
@@ -20,8 +36,35 @@ int main()
 	}
 	// 作成したウィンドウを OpenGL の処理対象にする
 	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Can't initialize GLEW" << std::endl;
+		return 1;
+	}
+
+	std::ostream * pipe = new std::ostream();
+	//std::ofstream * pipe;
+	
+	cy::TriMesh data;
+	data.LoadFromFileObj("teapot", true, pipe);
+
+	Vector3f Vertices[3241];
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	// The timing to wait for V-Sync
+	glfwSwapInterval(1);
+
 	// 背景色を指定する
-	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	// ウィンドウが開いている間繰り返す
 	while (glfwWindowShouldClose(window) == GL_FALSE)
 	{
@@ -29,6 +72,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		//
 		// ここで描画処理を行う
+		glDrawArrays(GL_POINTS, 0, 1);
 		//
 		// カラーバッファを入れ替える
 		glfwSwapBuffers(window);
